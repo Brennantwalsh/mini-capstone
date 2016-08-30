6ca69df7db2aa0d104a7d204d1d6580f616bcdb6
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-29 21:04:12 -0500

    faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  name = Faker::Commerce.product_name
+  price = Faker::Commerce.price
+  Product.create(name: name, price: price)
+end
