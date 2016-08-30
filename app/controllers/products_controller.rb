a3e5bde1ad7e14d4635cc84cfd1d45fb1a0884d1
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-29 20:16:58 -0500

    single product view added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def all
     @products = Product.all
   end
+
+  def single
+    @product = Product.find(1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/all' => 'products#all'
+  get '/single' => 'products#single'
 end
