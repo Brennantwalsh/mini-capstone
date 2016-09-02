8be48b8f164e0d00476448b7af7a98cf88817dee
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-01 20:17:31 -0500

    added mario with grammer errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,17 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+  end
+
+  def create
+    @product = Product.create(name: params[:name],
+                              price: params[:price],
+                              image: params[:image],
+                              description: params[:description])
+    render 'show.html.erb'
+  end
+
   def show
     @product = Product.find(params[:id])
   end
