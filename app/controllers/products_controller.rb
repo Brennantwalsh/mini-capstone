7fafc8a4c53723d7398cc030682370be5c7fe0bd
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-11 15:49:02 -0500

    searching

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,26 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    sort_attribute = params[:sort]
+    sort_order = params[:sort_order]
+    discount_level = params[:discount]
+    search_term = params[:search_term]
+
+    
+    if search_term
+      fuzzy_search_term = "%#{search_term}%"
+      @products = @products.where("name ILIKE ? OR description ILIKE ?", fuzzy_search_term, fuzzy_search_term)
+    end
+
+    if discount_level
+      @products = @products.where("price < ?", discount_level)
+    end
+
+    if sort_attribute && sort_order
+      @products = @products.order(sort_attribute => sort_order)
+    elsif sort_attribute
+      @products = @products.order(sort_attribute)
+    end
   end
 
   def new
@@ -40,5 +60,10 @@ class ProductsController < ApplicationController
     @product.destroy
     flash[:warning] = "IT is DEAD!"
     redirect_to '/products'
+  end
+
+  def random
+    random_product = Product.all.sample
+    redirect_to "/products/#{random_product.id}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909002838) do
+ActiveRecord::Schema.define(version: 20160911192646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 20160909002838) do
     t.string   "name"
     t.string   "image"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "price"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.decimal  "price",       precision: 8, scale: 2
     t.boolean  "in_stock"
   end
 
